f22e86412474e834ec23ee6b89c8a6e3a996f5e8
humminggbird <jdadzie@byupathway.edu>
2026-05-25 19:08:22 +0000

    Encapsulation development project

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,81 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        List<Scripture> scriptures = LoadScriptures();
+
+        Random random = new Random();
+
+        Scripture scripture = scriptures[random.Next(scriptures.Count)];
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.WriteLine();
+            Console.Write("Press Enter to continue or type 'quit' to finish: ");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+    }
+
+    static List<Scripture> LoadScriptures()
+    {
+        List<Scripture> scriptures = new List<Scripture>();
+
+        string[] lines = File.ReadAllLines("scriptures.txt");
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+
+            string book = parts[0];
+            int chapter = int.Parse(parts[1]);
+
+            Reference reference;
+
+            if (parts[2].Contains("-"))
+            {
+                string[] verses = parts[2].Split("-");
+
+                int startVerse = int.Parse(verses[0]);
+                int endVerse = int.Parse(verses[1]);
+
+                reference = new Reference(book, chapter, startVerse, endVerse);
+            }
+            else
+            {
+                int verse = int.Parse(parts[2]);
+
+                reference = new Reference(book, chapter, verse);
+            }
+
+            string text = parts[3];
+
+            Scripture scripture = new Scripture(reference, text);
+
+            scriptures.Add(scripture);
+        }
+
+        return scriptures;
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+        _words = new List<Word>();
+
+        string[] words = text.Split(" ");
+
+        foreach (string word in words)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+
+        List<Word> visibleWords = _words.Where(word => !word.IsHidden()).ToList();
+
+        for (int i = 0; i < numberToHide; i++)
+        {
+            if (visibleWords.Count == 0)
+            {
+                break;
+            }
+
+            int randomIndex = random.Next(visibleWords.Count);
+
+            visibleWords[randomIndex].Hide();
+
+            visibleWords.RemoveAt(randomIndex);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string scriptureText = "";
+
+        foreach (Word word in _words)
+        {
+            scriptureText += word.GetDisplayText() + " ";
+        }
+
+        return $"{_reference.GetDisplayText()} {scriptureText}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
