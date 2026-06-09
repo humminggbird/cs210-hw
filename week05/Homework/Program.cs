27fb0b62f64a4b86d1a1c661c8302eebd427143b
humminggbird <jdadzie@byupathway.edu>
2026-06-09 13:45:47 +0000

    Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment math = new MathAssignment("Joel Dadzie", "Geometry", "6.4", "5-18");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Joel Dadzie", "Geography", "The Impact of Climate on Human Settlement");
+
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
