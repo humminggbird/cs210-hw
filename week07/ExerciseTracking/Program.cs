2f71aa819ba8d015cd6a2fe76ed5171e9c918818
humminggbird <jdadzie@byupathway.edu>
2026-06-16 23:06:52 +0000

    ExerciseTracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,21 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2026, 6, 16), 40, 4.5),
+            new Cycling(new DateTime(2026, 6, 15), 60, 18.0),
+            new Swimming(new DateTime(2026, 6, 14), 35, 30)
+            };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
